c9be31b72636271a43b4f9e25d035792ce398fd9
Saifuddintaj <52877085+Saifuddintaj@users.noreply.github.com>
2023-03-04 22:23:14 +0530

    Added feature to process df

--- naukri_api.py
+++ naukri_api.py
@@ -1,6 +1,10 @@
 import requests
 import json
 import re
+import pandas as pd
+from urllib.parse import quote_plus
+from sqlalchemy import create_engine
+import pyodbc
 
 TAG_RE = re.compile(r'<[^>]+>')
 
@@ -37,21 +41,44 @@ def extract(page):
         jobid = json_response['jobDetails'][i]['jobId']
         title = json_response['jobDetails'][i]['title']
         company = json_response['jobDetails'][i]['companyName']
+        company = re.sub(',','.',company)
         posted = json_response['jobDetails'][i]['footerPlaceholderLabel']
+        company = re.sub(',','.',company)
         description = json_response['jobDetails'][i]['jobDescription']
         description = remove_tags(description)
         description = re.sub(',', '.', description)
         location = json_response['jobDetails'][i]['placeholders'][2]['label']
+        location = re.sub(',','.',location)
         salary = json_response['jobDetails'][i]['placeholders'][1]['label']
+        salary = re.sub(',','',salary)
         experience = json_response['jobDetails'][i]['placeholders'][0]['label']
+        experience = re.sub(',','.',experience)
         jd_i = jobDetails(jobid, title, company, posted, description, location, salary, experience)
         jd += (jd_i.get_csv())
     return jd
 
 
 def load(pages):
-    output = 'jobid,title,company,posted,description,location,salary,experience \n'
+    output = ''
     for page in range(pages):
         data = extract(page)
         output += data
     return output
+
+
+def create_dataframe(pages):
+  csv = load(pages)
+  columns = ["jobid","title","company","posted","description","location","salary","experience"]
+  df = pd.DataFrame([row.split(',') for row in csv.split('\n')],columns=columns)
+  df = df.dropna()
+  return df
+
+
+def write_df_to_table(df):
+  cnxn = pyodbc.connect('DRIVER={SQL Server};;Server=tcp:sqldataengineering.database.windows.net,1433;Database=naukri;Uid=dbadmin;Pwd=0p9o8i7u6yS@;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+  cursor = cnxn.cursor()
+  for index, row in df.iterrows():
+    cursor.execute("INSERT INTO dbo.jobtest (jobid,title,company,posted,description,location,salary,experience) values(?,?,?,?,?,?,?,?)", row.jobid, row.title, row.company,row.posted,row.description,row.location,row.salary,row.experience)
+    cnxn.commit()
+    cursor.close()
+
