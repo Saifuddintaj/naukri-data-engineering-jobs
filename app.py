16eb5bb668e2d8e2f5610f1bb542355dd7768369
Saifuddintaj <52877085+Saifuddintaj@users.noreply.github.com>
2023-03-04 22:34:55 +0530

    Added load dataframe to table end point.

--- app.py
+++ app.py
@@ -6,17 +6,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    if request.method == 'GET':
-        data = 'hello world'
+  if request.method == 'GET':
+    data = 'hello world'
     return jsonify({'data' : data})
 
 
 @app.route('/get_data/<int:no_of_pages>', methods=['GET'])
 def get_data(no_of_pages):
-    data = naukri_api.load(no_of_pages)
-    return data
+  data = naukri_api.load(no_of_pages)
+  return data
+
+@app.route('/load_data/<int:no_of_pages>', methods=['GET'])
+def load_data(no_of_pages):
+  df = naukri_api.create_dataframe(no_of_pages)
+  naukri_api.write_df_to_table(df)
+  return print(f"loaded {no_of_pages} of naukri data to database")
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+  app.debug = True
+  app.run()
